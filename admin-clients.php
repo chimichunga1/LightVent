87d31a88e9f73eecaab4eb942c6ee2e64d3cc427
wadewilson <miguel_ccortez@yahoo.com.ph>
2017-11-29 01:30:16 +0800

    almighty push

---
+++ admin-clients.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin | LightVend</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<?php include("admin-head-links.php"); ?>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+<?php include("admin-header.php"); ?>
+  <?php include("main-sidebar.php"); ?>
+
+<div class="content-wrapper">
+
+
+<div class="container">
+<div class="row">
+<div class="col-md-3">
+
+<br><br>
+             Filter By:    <!-- select -->
+                <div class="form-group">
+                
+                  <select class="form-control">
+                    <option>option 1</option>
+                    <option>option 2</option>
+                    <option>option 3</option>
+                    <option>option 4</option>
+                    <option>option 5</option>
+                  </select>
+                </div>
+                 <div class="form-group">
+
+                  <input type="text" class="form-control" placeholder="Search ...">
+                </div>       
+
+                <br>
+
+                  <button type="button" class="btn btn-block btn-primary">Add New Client</button>        
+
+
+</div>
+</div>
+
+
+<br><br>
+
+        <div class="col-xs-12">
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Clients Table</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Company Name</th>
+                  <th>Contact Person</th>
+                  <th>Tel # </th>
+                  <th>Mobile</th>
+           <th>Sold</th>
+
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0
+                  </td>
+  
+                  <td> 5</td>
+                  <td> 4</td>
+                  <td><button type="button" class="btn btn-block btn-success"><i class="fa fa-edit"></i></button></td>
+                  <td><button type="button" class="btn btn-block btn-danger"><i class="fa fa-close"></i></button>  </td>
+
+
+
+                </tr>
+
+
+
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>Asset Group</th>
+                  <th>Category</th>
+                  <th>Selling Price</th>
+                  <th>On Stock</th>
+           <th>Sold</th>
+
+                  <th>Action</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<?php include("admin-footer.php"); ?>
+
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+<!-- ./wrapper -->
+
+<?php include("admin-scripts.php"); ?>
+</body>
+</html>
---
+++ main-sidebar.php
@@ -0,0 +1,36 @@
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Xander Ford</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li><a href="admin-dashboard.php"><i class="fa fa-dashboard"></i> <span>DASHBOARD</span></a></li>
+        <li><a href="admin-supplier.php"><i class="fa fa-truck"></i> <span>SUPPLIERS</span></a></li>
+        <li><a href="admin-stock.php"><i class="fa fa-bar-chart"></i> <span>STOCK</span></a></li>
+        <li><a href="admin-group.php"><i class="fa fa-group"></i> <span>GROUP</span></a></li>
+        <li><a href="admin-sales-invoices.php"><i class="fa fa-dollar"></i> <span>SALES INVOICES</span></a></li>
+        <li><a href="admin-clients.php"><i class="fa fa-slideshare"></i> <span>CLIENTS</span></a></li>
+
+ <!-- 
+        <li class="header">LABELS</li>
+        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> -->
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
